4ace040ed5608bdbc66238e0f33173fef89e3f9e
Zeping Lee <zepinglee@gmail.com>
2019-05-02 15:34:14 +0800

    sync with ustcthesis

--- .ci/texlive.sh
+++ .ci/texlive.sh
@@ -11,12 +11,12 @@ if ! command -v tlmgr > /dev/null; then
     $INSTALL/install-tl -profile ./.ci/texlive.profile;
 fi
 
-tlmgr update --self --all --reinstall-forcibly-removed
+tlmgr update --self --all --reinstall-forcibly-removed;
 
 tlmgr install latexmk l3build \
     fontname fontspec l3kernel l3packages xetex \
     cjk ctex environ ms trimspaces ulem xecjk zhnumber \
-    caption filehook footmisc unicode-math \
-    fandol stix2-otf tex-gyre xits \
+    caption filehook footmisc notoccite titlesec unicode-math \
+    fandol tex-gyre xits \
     booktabs listings thumbpdf xcolor zapfding \
     algorithm2e ifoddpage nomencl relsize siunitx;
